6d77c141e5a5a6eeb68fec4af57748cbf27ce40e
Chad Simmons <csimmons@invoca.com>
2016-03-16 17:02:33 -0700

    additions for the dummy rails app to test storing in a field

---
+++ test/dummy/app/models/travel_itinerary.rb
@@ -0,0 +1,11 @@
+class TravelItinerary < ActiveRecord::Base
+  attr_accessible :aggregate_field
+
+  attr_accessible :estimated_cost
+  
+  include Aggregate::Container
+  self.aggregate_container_options[:use_storage_field] = :aggregate_field
+  
+  aggregate_attribute :estimated_cost, :decimal
+  aggregate_has_many :foreign_visits, "ForeignVisit"
+end
