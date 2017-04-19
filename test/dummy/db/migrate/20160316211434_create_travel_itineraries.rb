e1f34fad84806d979b2a64524151368367c2b57b
Bob Smith <bob@ringrevenue.com>
2017-04-18 17:17:30 -0700

    STORY-5105 - allow aggregates to work with Single Table Inheritance

--- test/dummy/db/migrate/20160316211434_create_travel_itineraries.rb
+++ test/dummy/db/migrate/20160316211434_create_travel_itineraries.rb
@@ -1,6 +1,7 @@
 class CreateTravelItineraries < ActiveRecord::Migration
   def change
     create_table :travel_itineraries do |t|
+      t.string :type, default: nil
       t.text :aggregate_field
 
       t.timestamps
